4c74a6218259af50da934097d48b37b0b1f1eb14
kakizaki hayate <hayate.h.0704@gmail.com>
2023-04-15 22:06:19 +0900

    初期画面

---
+++ QtlDataPass/QtlDataPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  QtlDataPassApp.swift
+//  QtlDataPass
+//
+//  Created by 柿崎逸 on 2023/04/15.
+//
+
+import SwiftUI
+
+@main
+struct QtlDataPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
